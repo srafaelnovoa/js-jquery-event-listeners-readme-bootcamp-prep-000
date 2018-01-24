14fdb032e2d6ae9058b87e13fc3ac9900d66a83a
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-24 21:18:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,40 @@
 $(document).ready(function(){
 
 // call functions here
+$('p').on("click",getIt);
+$('img').on("load",frameIt());
+$('#typing').on("keydown",pressIt);
+$('form').on("submit",submitIt);
 
 });
+
+
+function submitIt(){
+  alert("Your form is going to be submitted now.");
+}
+
+function getIt(){
+    alert("Hey!");
+}
+
+function frameIt(){
+  $('img').addClass("tasty")
+  return 
+}
+
+function pressIt(e) {
+   //key = parseInt(e.which);
+   //console.log(this)
+  //var key = window.event;
+  if (e || window.event)
+  {
+   
+  //console.log(window.event)
+  console.log(e.which)
+  if (parseInt(e.which)===71){
+      window.alert("G");
+    
+  }
+  }
+return 
+}
