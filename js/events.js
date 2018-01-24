3005d29b39c55eb80a35a7a84dd4f8f9cc28191a
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-24 21:36:37 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,35 @@
 $(document).ready(function(){
 
 // call functions here
+$('p').on("click",getIt);
+$('img').on("load",frameIt());
+$('form').on("submit",submitIt);
 
 });
+
+
+function submitIt(){
+  alert("Your form is going to be submitted now.");
+}
+
+function getIt(){
+    alert("Hey!");
+}
+
+function frameIt(){
+  $('img').addClass("tasty")
+  return 
+}
+
+function pressIt() {
+  $('#typing').on("keydown",function(e){
+      if (e || window.event)
+      {
+        console.log(e.which)
+        if (parseInt(e.which)===71){
+          window.alert("G");
+        }
+      }
+      });
+}
+pressIt();
